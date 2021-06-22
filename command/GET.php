5c42877906795775fe340fa8a92d36b60d0f5d99
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-06-22 22:00:31 +0900

    GETの場合のコードを記述

--- command/GET.php
+++ command/GET.php
@@ -1,3 +1,17 @@
 <?php
- echo 'Hello World!!';
-?>
+
+$id = $_GET['id'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=User_data;charset=utf8', 'staff', 'password');
+$sql = $pdo->prepare('select * from user_information where user_id=?');
+$sql->execute([$id]);
+
+if (!empty($sql->fetchAll())) {
+    $sql->execute([$id]);
+    foreach ($sql as $row) {
+        echo $row['name'], "\n";
+        echo $row['age'], "\n";
+    }
+}else{
+    echo '対象のレコードが見つかりません';
+}
